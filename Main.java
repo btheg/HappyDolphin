37c6f9c7589c9703f1a9d6c62f92b21e73903a32
DESKTOP-H6SJVQ1\Martin <mart963g@stud.kea.dk>
2017-11-27 08:58:24 +0100

    Indkapslede hver menu i deres egen funktion, på Martin branch

--- Main.java
+++ Main.java
@@ -25,7 +25,7 @@ public class Main
         //System.out.println(wuddup.svømmere);
 
         Menu menu = new Menu();
-        menu.menu();
+        menu.menuLogin();
 
 
     }
--- Menu.java
+++ Menu.java
@@ -5,18 +5,22 @@ public class Menu
 {
     private SvømmerListe svømmerListe = new SvømmerListe();
     private HoldListe hold = new HoldListe();
+    private DisciplinListe discipliner = new DisciplinListe();
+    private Scanner scanner = new Scanner(System.in);
+
     public Menu()
     {
     }
 
 
-    public void menu() {
+    public void menuLogin()
+    {
         hold.init();
-        Scanner scanner = new Scanner(System.in);
+        discipliner.init();
 
-        Ansat ansat1 = new Ansat("Ole","mail",20,"Formand","123");
-        Ansat ansat2 = new Ansat("Gert","mail",40,"Kasserer","123");
-        Ansat ansat3 = new Ansat("John","mail",55,"Træner","123");
+        Ansat ansat1 = new Ansat("Ole", "mail", 20, "Formand", "123");
+        Ansat ansat2 = new Ansat("Gert", "mail", 40, "Kasserer", "123");
+        Ansat ansat3 = new Ansat("John", "mail", 55, "Træner", "123");
 
         // Welcome Message
         System.out.println("Velkommen til svømmeklubben Delfinen");
@@ -33,16 +37,51 @@ public class Menu
                 System.out.println("Enter Password");
                 String korrektPassword = scanner.nextLine();
 
-                if (ansat1.getPassWord().equals(korrektPassword)){
+                if (ansat1.getPassWord().equals(korrektPassword))
+                {
                     korrektlogin = true;
+                    // Formandens Menu
+                    this.formandMenu();
+
                 }
             }
+
+            if (ansat2.getUserName().equals(korrektEmail))
+            {
+                System.out.println("Enter Password");
+                String korrektPassword = scanner.nextLine();
+
+                if (ansat2.getPassWord().equals(korrektPassword))
+                {
+                    korrektlogin = true;
+                    // Kasserer menu
+                    this.kasserMenu();
+                }
+            }
+
+
+            if (ansat3.getUserName().equals(korrektEmail))
+            {
+                System.out.println("Enter Password");
+                String korrektPassword = scanner.nextLine();
+
+                if (ansat3.getPassWord().equals(korrektPassword))
+                {
+                    korrektlogin = true;
+                    // Træner menu
+                    this.trænerMenu();
+                }
+            }
+
+
         }
+    }
 
-        // Formandens Menu
-
-        boolean isRunning = true;
-        while (isRunning) {
+    public void formandMenu()
+    {
+        boolean isRunningFormand = true;
+        while (isRunningFormand)
+        {
 
             System.out.println("Menu\n" +
                     "1. Opret Medlem\n" +
@@ -52,7 +91,7 @@ public class Menu
                     "Press any other key to exit");
 
             String valg = scanner.next();
-            switch(valg)
+            switch (valg)
             {
                 case "1":
                     System.out.println("Indtast medlemmets navn");
@@ -63,7 +102,7 @@ public class Menu
                     boolean wronginput = true;
                     int alder = 0;
                     boolean konkurrence = true;
-                    while(wronginput)
+                    while (wronginput)
                     {
                         try
                         {
@@ -80,9 +119,11 @@ public class Menu
                         }
                     }
                     boolean rigtigthold = false;
-                    Svømmer nytmedlem = new Svømmer(navn,email,alder,konkurrence);
+                    Svømmer nytmedlem = new Svømmer(navn, email, alder, konkurrence);
                     boolean holdErIListen = false;
-                    while(!holdErIListen)
+
+
+                    while (!holdErIListen)
                     {
                         System.out.println("Indtast nummeret for ønskede hold: ");
                         for (int i = 0; i < hold.hold.size(); i++)
@@ -93,13 +134,12 @@ public class Menu
                         {
                             scanner.nextLine();
                             int choice = scanner.nextInt();
-                            if(hold.hold.get(choice)!=null)
+                            if (hold.hold.get(choice) != null)
                             {
                                 svømmerListe.tilføjSvømmer(nytmedlem, hold.hold.get(choice));
                                 holdErIListen = true;
                             }
-                        }
-                        catch (Exception e)
+                        } catch (Exception e)
                         {
                             System.out.println("Det skal være et tal fra listen!");
                         }
@@ -119,7 +159,7 @@ public class Menu
                     //Find medlem via mail
                     boolean isMedlem = false;
                     scanner.nextLine();
-                    while(!isMedlem)
+                    while (!isMedlem)
                     {
                         System.out.println("Medlemmer: ");
                         System.out.println(svømmerListe.svømmere);
@@ -143,16 +183,90 @@ public class Menu
                     System.out.println(svømmerListe.svømmere);
                     break;
                 default:
+                    isRunningFormand = false;
+                    break;
+            }
+        }
+    }
+
+    public void kasserMenu()
+    {
+        boolean isRunning = true;
+        while (isRunning)
+        {
+            System.out.println("Menu\n" +
+                    "1. Rediger Medlemskab\n" +
+                    "2. Se betalingsoversigt\n" +
+                    "3. Se Medlemmer\n" +
+                    "Press any other key to exit");
+
+            String valg = scanner.next();
+            switch (valg)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                default:
                     isRunning = false;
                     break;
             }
         }
-
-
-
     }
 
+    public void trænerMenu()
+    {
+        boolean isRunningTræner = true;
+        while (isRunningTræner)
+        {
+            System.out.println("Menu\n" +
+                    "1. Registrer resultater\n" +
+                    "2. Se Konkurrence Svømmere\n" +
+                    "3. Se Rangliste\n" +
+                    "4. Top 5\n" +
+                    "Press any other key to exit");
 
+            String valg = scanner.next();
+            switch (valg)
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
 
+                    boolean disciplinerErPåListen = false;
+                    while(!disciplinerErPåListen)
+                    {
+                        System.out.println("Hvilken diciplin vil du se top 5 af?");
+                        for (int i = 0; i < discipliner.discipliner.size(); i++)
+                        {
 
+                            System.out.println(i + ". " + discipliner.discipliner.get(i).type);
+                        }
+                        try
+                        {
+                            scanner.nextLine();
+                            int choice = scanner.nextInt();
+                            if (discipliner.discipliner.get(choice) != null)
+                            {
+
+                                disciplinerErPåListen = true;
+                            }
+                        } catch (Exception e)
+                        {
+                            System.out.println("Det skal være en disciplin fra listen!");
+                        }
+                    }
+                    break;
+                default:
+                    isRunningTræner = false;
+                    break;
+            }
+        }
+    }
 }
